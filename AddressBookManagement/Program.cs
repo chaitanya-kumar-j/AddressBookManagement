3ff14123dfac18173ff92eb7025ba38569e11aa3
chaitanya4294 <chaitanya.4294@gmail.com>
2021-09-28 13:59:34 +0530

    [Chaitanya] Add : Added Folder structure and welcome message

---
+++ AddressBookManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hello World!...\n Welcome to address book management system!....");
+        }
+    }
+}
