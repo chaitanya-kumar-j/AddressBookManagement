4c20609e4e40b368d22ddefbe57fbe154b17405e
chaitanya4294 <chaitanya.4294@gmail.com>
2021-09-28 14:04:37 +0530

    [Chaitanya] Add : Added contact creation method

---
+++ AddressBookManagement/AddressBooks.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookManagement
+{
+    public class AddressBooks
+    {
+        // method to create new contact
+        public void CreateContact()
+        {
+            Contact contact = new Contact();
+
+            Console.WriteLine("Enter the first name of the contact: ");
+            contact.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter the second name of the contact: ");
+            contact.SecondName = Console.ReadLine();
+
+            Console.WriteLine("Enter the Address of the contact seperated by comma: ");
+            contact.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter the City of the contact: ");
+            contact.City = Console.ReadLine();
+
+            Console.WriteLine("Enter the State of the contact: ");
+            contact.State = Console.ReadLine();
+
+            Console.WriteLine("Enter the Zip of the contact: ");
+            contact.ZipCode = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Phone number of the contact: ");
+            contact.PhoneNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the email id of the contact: ");
+            contact.Email = Console.ReadLine();
+
+        }
+    }
+}
--- AddressBookManagement/Program.cs
+++ AddressBookManagement/Program.cs
@@ -6,7 +6,43 @@ namespace AddressBookManagement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Hello World!...\n Welcome to address book management system!....");
+            // Run or exit
+            bool isRun = true;
+            AddressBooks books = new AddressBooks();
+            while (isRun)
+            {
+                Console.WriteLine(" Hello World!...\n Welcome to address book management system!....");
+                Console.WriteLine(" Select and enter query number: " +
+                    "(1. Create contact list, 2. Add new contact, 3. Edit existing contact, " +
+                    "4. Delete contact by person name, 5. Add Multiple persons, " +
+                    "6. Multiple addressbooks with unique name)");
+                int selectProgram = Convert.ToInt32(Console.ReadLine());
+                switch (selectProgram)
+                {
+                    case 1:
+                        books.CreateContact();
+                        break;
+                    case 2:
+                        // Add new Contact
+                        break;
+                    case 3:
+                        // Edit contact by name
+                        break;
+                    case 4:
+                        // Delete contact
+                        break;
+                    case 5:
+                        // Add multiple persons
+                        break;
+                    case 6:
+                        // Multi Addressbooks
+                        break;
+                    default:
+                        // exit program
+                        isRun = !isRun;
+                        break;
+                }
+            }
         }
     }
 }
